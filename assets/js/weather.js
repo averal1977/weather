88ae7fb91ac83f41b5c6c64058e231a7ddd5c829
averal1977 <122688505+averal1977@users.noreply.github.com>
2023-02-14 01:53:31 -0500

    AppWeather 3.0

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,9 +1,16 @@
 
 import { weather_data } from './data.js';
 
-let {city_code, city, date, maxtemperature, mintemperature, cloudiness, wind, rainfall, forecast_today, forecast_week} = weather_data[0] 
 
-let loadDayForecastData = () => {
+
+let loadDayForecastData = (ciudadSeleccionada) => {
+    let dataCiudad = weather_data.find( e => e.city === ciudadSeleccionada);
+    
+    let {city, maxtemperature, mintemperature, cloudiness, wind, rainfall, forecast_today} = dataCiudad;
+    let [late, night] = forecast_today;
+    let { text: textLate, temperature: temperatureLate, forecast: forecastLate, icon: iconLate } = late;
+    let { text: textNight, temperature: temperatureNight, forecast: forecastNight, icon: iconNight } = night;
+
 	let ciudad = document.getElementById ('city');
     ciudad.innerHTML = city;
 
@@ -25,38 +32,83 @@ let loadDayForecastData = () => {
     let lluvia = document.getElementById ('rainfall');
     lluvia.innerHTML = rainfall;
 
+   //---------------------------
+    let lateIcon = document.getElementById("late_icon");
+    lateIcon.innerText = iconLate;
+    
+    let lateTemperature = document.getElementById("late_temperature");
+    lateTemperature.innerText = temperatureLate;
+
+    let lateForecast = document.getElementById("late_forecast");
+    lateForecast.innerText = forecastLate;
+
+    let lateText = document.getElementById("late_text");
+    lateText.innerText = textLate;
+
+    let nightIcon = document.getElementById("night_icon");
+    nightIcon.innerText = iconNight;
+
+    let nightTemperature = document.getElementById("night_temperature");
+    nightTemperature.innerText = temperatureNight;
+
+    let nightForecast = document.getElementById("night_forecast");
+    nightForecast.innerText = forecastNight;
+
+    let nightText = document.getElementById("night_text");
+    nightText.innerText = textNight;
+
     // console.log(ciudad);
 }
 
-let loadWeekForecastData = () => {
-    let {day, text, date, temperature : { min, max }, icon} = forecast_week[0];	
-    // let {min, max} = temperature;
-    let listOfForecast= document.getElementsByClassName('list-group') ;
-    let contenido = `<li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-    <div class="d-flex flex-column">
-    <h6 class="mb-1 text-dark font-weight-bold text-sm">${text}</h6>
-    <span class="text-xs">${date}</span>
-    </div>
-    <div class="d-flex align-items-center ">
-    <span class="font-weight-bold text-dark mx-2">${max}</span> |  <span class="text-dark mx-2">${min}</span>
-    <div class="ms-4"><i class="material-icons fs-2 me-1 rainy">${icon}</i></div>
-    </div>
-    </li>`;    
-    listOfForecast[0].innerHTML = contenido
+let loadWeekForecastData = (ciudadSeleccionada) => {
+    let dataCiudad = weather_data.find( e => e.city === ciudadSeleccionada);
+    let {forecast_week} = dataCiudad;
+    let ListForecastWeek = document.getElementById("forecastweek");
+    let contenido = '';
+   
+    forecast_week.map(e => {
+        let {text, date, temperature : { min, max }, icon} = e;	
+        contenido += `<li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                    <div class="d-flex flex-column">
+                    <h6 class="mb-1 text-dark font-weight-bold text-sm">${text}</h6>
+                    <span class="text-xs">${date}</span>
+                    </div>
+                    <div class="d-flex align-items-center ">
+                    <span class="font-weight-bold text-dark mx-2">${max}</span> |  <span class="text-dark mx-2">${min}</span>
+                    <div class="ms-4"><i class="material-icons fs-2 me-1 rainy">${icon}</i></div>
+                    </div>
+                </li>`;     
+    })
+    ListForecastWeek.innerHTML = contenido;
     // console.log(contenido);
+}
+
+let loadCiudad = () => {
+    let objCiudades = document.getElementById('dropdownMenuButton');
+    let ciudades = weather_data.map(e => e.city);
+    ciudades.map(e => {
+        objCiudades.innerHTML += `<option class="dropdown-item" value="${e}">${e}</option>`;
+    });
 }
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    loadCiudad();
     loadDayForecastData('Guayaquil');
-});
+})
 
+let btnCiudad = document.getElementById("dropdownMenuButton");
+btnCiudad.addEventListener('change', (event) => {
+    let ciudadSeleccionada = event.target.value;
+    document.getElementById("forecastweek").innerHTML = '';
+    loadDayForecastData(ciudadSeleccionada);
+})
 
 let botonCargar = document.getElementById("loadinfo");
 botonCargar.addEventListener('click', (event) => {
-    loadWeekForecastData();
+    //  loadWeekForecastData();
+    let ciudadSeleccionada = document.getElementById("dropdownMenuButton").value;
+    loadWeekForecastData(ciudadSeleccionada);    
 });
-
-
 
